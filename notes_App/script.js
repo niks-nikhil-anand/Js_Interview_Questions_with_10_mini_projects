85a3567695764b67a438537837f6239c765cece4
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-23 21:57:42 +0530

    added li delete and done features

--- notes_App/script.js
+++ notes_App/script.js
@@ -2,13 +2,24 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 
-function addTask(){
-    if(inputBox.value === ""){
+function addTask() {
+    if (inputBox.value === "") {
         alert("You must write something")
-    }else{
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.append(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+} , false);
