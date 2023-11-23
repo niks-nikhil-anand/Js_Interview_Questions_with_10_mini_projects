6b8c90059bd6de7f32ecb488d67d558a9cbc063f
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-23 21:17:43 +0530

    modified add button

---
+++ notes_App/script.js
@@ -0,0 +1,14 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must write something")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+    }
+    inputBox.value = "";
+}
