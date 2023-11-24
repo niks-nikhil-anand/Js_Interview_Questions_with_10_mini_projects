bb0dba4f1323ae289f31a60b0a479fd62fd910d8
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-24 18:30:58 +0530

    Quiz App completed

--- quiz_app/script.js
+++ quiz_app/script.js
@@ -1,51 +1,51 @@
 const questions = [
     {
-        questions:"JavaScript is a loosely typed language.",
-        answers:[
-            {text: "True" , correct: false},
-            {text: "False" , correct: false},
-            {text: " It depends on the context" , correct: false},
-            {text: "JavaScript is both loosely and dynamically typed" , correct: true},
+        questions: "JavaScript is a loosely typed language.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: false },
+            { text: " It depends on the context", correct: false },
+            { text: "JavaScript is both loosely and dynamically typed", correct: true },
 
         ]
     },
     {
-        questions:" The localStorage object in JavaScript has the same storage limit as the sessionStorage object.",
-        answers:[
-            {text: "True" , correct: true},
-            {text: "False" , correct: false},
-            {text: " It depends on the browser" , correct: false},
-            {text: "`localStorage and sessionStorage` have unlimited storage" , correct: false},
+        questions: " The localStorage object in JavaScript has the same storage limit as the sessionStorage object.",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false },
+            { text: " It depends on the browser", correct: false },
+            { text: "`localStorage and sessionStorage` have unlimited storage", correct: false },
 
         ]
     },
     {
-        questions:" JavaScript promises are always asynchronous.",
-        answers:[
-            {text: "True" , correct: false},
-            {text: "False" , correct: true},
-            {text: " Depends on the implementation" , correct: false},
-            {text: "Promises can't be used for asynchronous operations" , correct: false},
+        questions: " JavaScript promises are always asynchronous.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true },
+            { text: " Depends on the implementation", correct: false },
+            { text: "Promises can't be used for asynchronous operations", correct: false },
 
         ]
     },
     {
-        questions:" The event.stopPropagation() method prevents the event from propagating up or down the DOM tree.",
-        answers:[
-            {text: "True" , correct: true},
-            {text: "False" , correct: false},
-            {text: " It only prevents propagation up the DOM tree" , correct: false},
-            {text: " It only prevents propagation down the DOM tree" , correct: false},
+        questions: " The event.stopPropagation() method prevents the event from propagating up or down the DOM tree.",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false },
+            { text: " It only prevents propagation up the DOM tree", correct: false },
+            { text: " It only prevents propagation down the DOM tree", correct: false },
 
         ]
     },
     {
-        questions:" The map() function in JavaScript modifies the original array.",
-        answers:[
-            {text: "True" , correct: false},
-            {text: "False" , correct: true},
-            {text: " It depends on the implementation" , correct: false},
-            {text: "The map() function creates a new array without modifying the original" , correct: false},
+        questions: " The map() function in JavaScript modifies the original array.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true },
+            { text: " It depends on the implementation", correct: false },
+            { text: "The map() function creates a new array without modifying the original", correct: false },
 
         ]
     },
@@ -58,17 +58,17 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
-function startQuiz(){
-    currentQuestionIndex =0;
+function startQuiz() {
+    currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestions();
 }
 
-function showQuestions(){
+function showQuestions() {
     resetState();
-   let currentQuestion = questions[currentQuestionIndex];
-    let questionsNo = currentQuestionIndex +1;
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionsNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionsNo + "." + currentQuestion.questions;
 
     currentQuestion.answers.forEach(answer => {
@@ -76,55 +76,59 @@ function showQuestions(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
-        if(answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click" , selectAnswer);
+        button.addEventListener("click", selectAnswer);
     })
 }
 
-function resetState(){
-   nextButton.style.display = "none";
-   while(answerButton.firstChild){
-    answerButton.removeChild(answerButton.firstChild);
-   } 
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
 }
 
-function selectAnswer(dets){
+function selectAnswer(dets) {
     const selectedBtn = dets.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
-    }else{
+    } else {
         selectedBtn.classList.add("incorrect");
     }
-    Array.from(answerButton.children).forEach(button =>{
-        if(button.dataset.correct === "true"){
+    Array.from(answerButton.children).forEach(button => {
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
         button.disabled = true;
     });
     nextButton.style.display = "block";
- }
+}
 
- function showScore(){
-
- }
-function handleNextButton(){
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+    
+}
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex<questions.length){
+    if (currentQuestionIndex < questions.length) {
         showQuestions();
-    }else{
+    } else {
         showScore();
     }
 
 }
- nextButton.addEventListener("click" ,()=> {
-    if(currentQuestionIndex<questions.length){
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
         handleNextButton();
-    }else{
+    } else {
         startQuiz();
     }
- })
+})
 startQuiz();
