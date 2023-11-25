48a449b9d710bd69a8410fffc57159e863d65b9b
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-25 21:49:03 +0530

    added annimation in the input field

--- qr_code/script.js
+++ qr_code/script.js
@@ -7,8 +7,12 @@ function generateQr() {
     if (input_text.value.length > 0) {
         qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input_text.value;
         imgBox.classList.add("show-img");
-    }else{
-        alert("Enter something to generate Qr code ")
+    } else {
+        // alert("Enter something to generate Qr code ")
+        input_text.classList.add("error");
+        setTimeout(() => {
+            input_text.classList.remove("error");
+        }, 1000);
     }
 
 }
