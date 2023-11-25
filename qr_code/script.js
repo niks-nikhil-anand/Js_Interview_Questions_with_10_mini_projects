b5f8950a4a8997ce88223f5beedeebb15334d17d
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-25 21:40:50 +0530

    added some css and js for the qr code

--- qr_code/script.js
+++ qr_code/script.js
@@ -3,6 +3,12 @@ let qr_image = document.getElementById("qr_image");
 let input_text = document.getElementById("input_text");
 
 
-function generateQr(){
-    qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +  input_text.value;
+function generateQr() {
+    if (input_text.value.length > 0) {
+        qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input_text.value;
+        imgBox.classList.add("show-img");
+    }else{
+        alert("Enter something to generate Qr code ")
+    }
+
 }
