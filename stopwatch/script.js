5e3a4f534b8a6ef71174b1ddbb679164c036db62
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-26 10:47:05 +0530

    stopwatch completed

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,6 +1,7 @@
 let [seconds , minutes , hours] = [0,0,0];
 
 let displayTime = document.getElementById("displayTime");
+let timer = null;
 
 function stopwatch(){
     seconds++;
@@ -12,4 +13,25 @@ function stopwatch(){
             hours++;
         }
     }
+    let h = hours<10 ? "0" + hours : hours;
+    let m = minutes<10 ? "0" + minutes : minutes;
+    let s = seconds<10 ? "0" + seconds : seconds;
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart(){
+    if(timer != null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+function watchReset(){
+    clearInterval(timer);
+    [seconds , minutes , hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+
 }
