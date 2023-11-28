b2c836b7078fcc9b361805d6b280739602e6606e
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-11-28 16:04:11 +0530

    finally , completed with no error

--- email_subscription/script.js
+++ email_subscription/script.js
@@ -1,9 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxNiSAPM2QEZeV1Ra5XHIGLAXm7yJ1LEhjOPlI6aV4ElfPKsJJkAKkVT6jV285tBA/exec'
-  const form = document.forms['submit-to-google-sheet']
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response =>{
+            msg.innerHTML = " Thank You for subscribing!";
+            setTimeout(function(){
+                msg.innerHTML = "";
+            } , 5000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
