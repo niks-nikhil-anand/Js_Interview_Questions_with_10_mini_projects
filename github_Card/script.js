ce2c5950b1234d71977b3742c5ec8d099084a3c2
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-12 06:29:40 +0530

    script.js completed

--- github_Card/script.js
+++ github_Card/script.js
@@ -7,17 +7,41 @@ document.getElementById("myForm").addEventListener("submit" , function(event)  {
 
 
 
-async function getData(username){
+async function getData(username) {
     await fetch(`https://api.github.com/users/${username}`)
-    .then((res)=> res.json())
-    .then((res) => {
-        let name = document.querySelector(".name")
-        name.innerHTML = `<h4>${username}</h4>`;
-    
-        let image = document.querySelector(".avtaar")
-        image.innerHTML = `<img src="${res.avatar_url}" alt="">` 
-    })
-    
+        .then((res) => res.json())
+        .then((res) => {
+            let name = document.querySelector(".name");
+            name.innerHTML = `<h4>${username}</h4>`;
+
+            let image = document.querySelector(".avtaar");
+            image.innerHTML = `<img src="${res.avatar_url}" alt="avataar">`;
+
+            let list = document.querySelector(".list");
+
+           
+            list.innerHTML = `<li>
+                <h3>Full Name</h3>
+                <h4>${res.name}</h4>
+            </li>
+            <li>
+                <h3>Username:- </h3>
+                <h4>${res.login}</h4>
+            </li>
+            <li>
+                <h3>Location:-</h3>
+                <h4>${res.location}</h4>
+            </li>
+            <li>
+                <h3>Total Followers:- </h3>
+                <h4>${res.followers}</h4>
+            </li>
+            <li>
+                <h3>About:- </h3>
+                <h4>${res.bio}</h4>
+            </li>`;
+        })
+        .catch((error) => console.error('Error fetching GitHub user data:', error));
 }
 
 
