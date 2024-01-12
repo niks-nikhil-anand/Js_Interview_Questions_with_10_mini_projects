37cbf657c13fb72a3bde5a6e81fa8c6e56c9591e
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-12 09:51:31 +0530

    markdown file

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Hello this is nikhil.
+**10)- What is Immediately Invoked Functions.?**
+
+
+>Keep Smiling and work harder
+
---
+++ js_interview_prep/script.js
@@ -0,0 +1,5 @@
+// Immediately Invoked Functions(IIFY)
+
+(function(){
+    console.log("hello this is Immediately invoked functions")
+})();
